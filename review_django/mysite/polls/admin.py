3d0b3a082ec822181fa929ed972e56248c6f4bad
younglaecho <tks7205@gmail.com>
2022-01-15 16:55:37 +0900

    hide secret Key

--- review_django/mysite/polls/admin.py
+++ review_django/mysite/polls/admin.py
@@ -1,5 +1,26 @@
 from django.contrib import admin
 from polls.models import Question, Choice
 
-admin.site.register(Question)
+
+# class ChoiceInline(admin.StackedInline):
+#   model = Choice
+#   extra = 2
+
+class ChoiceInline(admin.TabularInline):
+  model = Choice
+  extra = 2
+   
+class QuestionAdmin(admin.ModelAdmin):
+  # fields = ['pub_date', 'question_text']
+  fieldsets = [
+    (None              , {"fields": ['question_text']}),
+    ('Date Information', {"fields": ['pub_date'], 'classes':['collapse']  }),
+  ]
+  inlines = [ChoiceInline]
+  
+  list_display = ('question_text', 'pub_date')
+  list_filter = ['pub_date']
+  search_fields = ['question_text']
+
+admin.site.register(Question, QuestionAdmin)
 admin.site.register(Choice)
--- review_django/mysite/settings.py
+++ review_django/mysite/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import os, json
-from django.core.exceptions import ImproperlyConfigured
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -22,20 +21,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-
-secret_file = os.path.join(BASE_DIR, 'secrets.json' ) 
+secret_file = os.path.join(BASE_DIR, 'secrets.json') 
 with open(secret_file) as f:
-    secrets = json.loads(f.read() )
+    secrets = json.loads(f.read())
 
-def get_secret(setting, secrets=secrets):
-    """비밀 변수를 가져오거나 명시적 예외를 반환한다."""
-    try:
-        return secrets[setting]
-    except KeyError:
-        error_msg = "Set the {} environment variable".format(setting)
-        raise ImproperlyConfigured(error_msg)
-
-SECRET_KEY = get_secret("SECRET_KEY")
+SECRET_KEY = 'django-insecure-(b#g!s43_(0k6+_q3bz^6@ah^!b2qjbwn5x1c#42$wp=kaqxjw'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
